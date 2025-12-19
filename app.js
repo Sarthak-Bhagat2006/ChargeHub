891dd91006a7f3e2f3f78b081e48d4d318a6876e
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2025-12-19 16:53:21 +0530

    Change in entry point of APP

--- app.js
+++ app.js
@@ -109,6 +109,10 @@ app.use((req, res, next) => {
 //    res.send(registerUser);
 // })
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 app.use("/", users);
