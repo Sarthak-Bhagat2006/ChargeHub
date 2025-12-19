0bc3e611cee76e8cd4ff71a62bdaf7e722656fb5
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2025-12-19 18:34:22 +0530

    Changes in country and login and signup routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,51 +1,46 @@
 const User = require("../Models/user");
 
-//signupRender
-
+// Render Signup Page
 module.exports.signupRender = (req, res) => {
     return res.render("users/signup");
 }
 
+// Signup / Register new user
 module.exports.signin = async (req, res, next) => {
     try {
-        let { username, email, password } = req.body;
+        const { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
+
+        // Use req.login safely
         req.login(registeredUser, (err) => {
             if (err) {
                 return next(err);
             }
             req.flash("success", "Welcome to ChargeHub");
-            res.redirect("/listings");
+            return res.redirect("/listings");
         });
 
     } catch (e) {
         req.flash("error", e.message);
-        res.redirect("/signup");
+        return res.redirect("/signup");
     }
 }
 
-
-
+// Login existing user
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome to ChargeHub');
-    let redirectUrl = res.locals.redirectUrl || "/listings"
-    res.redirect(redirectUrl);
+    req.flash("success", "Welcome to ChargeHub");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    return res.redirect(redirectUrl);
 }
 
-
-
-
+// Logout user
 module.exports.logout = (req, res, next) => {
     req.logout((err) => {
         if (err) {
             return next(err);
         }
-        req.flash("success", "You are logout now");
-        res.redirect("/listings");
+        req.flash("success", "You are logged out now");
+        return res.redirect("/listings");
     });
 }
-
-
-
